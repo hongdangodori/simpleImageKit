868332f0f74c9d8098d0da275016cc5bab049e02
chungdan <chungdan@coupang.com>
2018-03-19 21:48:59 +0900

    create SimpleImageKit project

---
+++ SimpleImageKit/SimpleImageKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleImageKit
+//
+//  Created by 한병호 on 2018. 3. 19..
+//  Copyright © 2018년 한병호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
